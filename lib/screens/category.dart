8a38b1ee42eabbf07baaad069dc6a6fff16233c6
IamHaydenNg <ngheiyue.dee@gmail.com>
2024-07-23 11:05:57 +0800

    Add code of class 162

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/models/category.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
-    Navigator.push(
-      context,
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(title: "some title", meals: []),
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
       ),
     );
   }
@@ -34,7 +41,7 @@ class CategoryScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             )
         ],
